fc49aa333e1534f60963be75e8476429123ac22b
zengjunjie　 <zengjunjie@thinkenergy.tech>
2022-06-09 18:00:38 +0800

    初始化项目

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+'''
+@author: zengjunjie
+@contact: zengjunjie@thinkenergy.tech
+@software: Pycharm
+@application:
+@file: setup.py
+@time: 09/06/22 17:57
+@desc:
+'''
+
+import setuptools
+
+setuptools.setup()
+
